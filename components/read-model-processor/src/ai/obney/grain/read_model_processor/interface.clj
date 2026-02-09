49b8b751a7803d321f197536b2bee2658dfc0b2b
cjbarre <cjbarre@gmail.com>
2026-02-09 14:32:45 -0800

    Delegate to core

--- components/read-model-processor/src/ai/obney/grain/read_model_processor/interface.clj
+++ components/read-model-processor/src/ai/obney/grain/read_model_processor/interface.clj
@@ -1,9 +1,11 @@
-(ns ai.obney.grain.read-model-processor.interface)
+(ns ai.obney.grain.read-model-processor.interface
+  (:require [ai.obney.grain.read-model-processor.core :as core]))
 
 (defn p
   "Project a Read Model.
-   
+
    The Read Model Processor transparently manages
    incremental snapshotting and caching."
-  [{:keys [_file-store _event-store] :as _context}
-   {:keys [_f _query _name _version] :as _args}])
+  [{:keys [_file-store _event-store] :as context}
+   {:keys [_f _query _name _version] :as args}]
+  (core/p context args))
